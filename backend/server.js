1c8f3cddce22d73bbcc2af98093fa93a41042b4d
lich <lichshai999@gmail.com>
2026-04-22 13:09:20 +0800

    fix frontend path

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from frontend folder (parent directory)
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Import routes
 const logsRouter = require('./routes/logs');
@@ -28,7 +28,7 @@ app.use('/api/labs', labsRouter);
 
 // Default route - serve index.html from parent directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // Error handling middleware
